abc3af0c5799da6fe48d6d6ae16acec1711e859f
mradambeck <mr.adambeck@gmail.com>
2016-04-10 14:44:02 -0700

    renders all suggestions on load

--- public/scripts/events.js
+++ public/scripts/events.js
@@ -2,15 +2,18 @@
 // CLIENT SIDE JAVASCRIPT //
 ////////////////////////////
 
-// takes URL, grabs eventID from the end:
+// takes URL, grabs eventID from the end, cretes API paths:
 var pathname = window.location.pathname,
 pathSplit = pathname.split('/'),
 eventID = pathSplit.pop(),
 urlCall = ('/api/events/' + eventID); // creates API call URL
+suggUrlCall = ('/api/events/' + eventID + '/suggestions'); // creates API call for Suggestions URL
 console.log('eventID: ', eventID);
+console.log('suggUrlCall: ', suggUrlCall);
 
 var eventData,
 suggestionData,
+suggOnLoadData,
 $eventTarget,
 $suggestionTarget;
 
@@ -24,6 +27,7 @@ $(document).ready(function() {
   $eventTarget = $('#event-target');
   $suggestionTarget = $('#suggestions-target');
 
+
   // handlebars compile
   var eventSource = $('#event-template').html();
   eventTemplate = Handlebars.compile(eventSource);
@@ -31,6 +35,11 @@ $(document).ready(function() {
   var suggestionsSource = $('#suggestions-template').html();
   suggestionsTemplate = Handlebars.compile(suggestionsSource);
 
+  var suggOnLoadSource = $('#sugg-onLoad-template').html();
+  suggOnLoadTemplate = Handlebars.compile(suggOnLoadSource);
+
+
+  // Populate Events on load
   $.ajax({
     method: 'GET',
     url: urlCall,
@@ -38,11 +47,20 @@ $(document).ready(function() {
     error: handleError
   });
 
-  $('#suggest-btn').on('click', function(){
-    $('#suggest-modal').modal();
-
+  // Populate Suggestions on load
+  $.ajax({
+    method: 'GET',
+    url: suggUrlCall,
+    success: suggOnLoadSuccess,
+    error: suggOnLoadError
   });
 
+  // Open Suggestion modal
+  $('#suggest-btn').on('click', function(){
+    $('#suggest-modal').modal();
+  });
+
+  // Suggestions submission handling
   $('#suggest-submit-btn').on('click', function(e) {
     e.preventDefault();
     console.log('new suggestion button clicked');
@@ -71,16 +89,30 @@ $(document).ready(function() {
 
 });
 
+
+///////////////
+// FUNCTIONS //
+///////////////
+
+// handlebars rendering of event data
 function renderEvent(){
   var eventHtml = eventTemplate({event: eventData});
   $eventTarget.append(eventHtml);
 }
 
+// handlebars rendering of a new suggestion card
 function renderSuggestion(){
   var suggestionHtml = suggestionsTemplate({suggestion: suggestionData});
   $suggestionTarget.append(suggestionHtml);
 }
 
+// handlebars rendering of all suggestions on load
+function renderSuggestions(){
+  var suggestionsHtml = suggOnLoadTemplate({suggestions: suggOnLoadData});
+  $suggestionTarget.append(suggestionsHtml);
+}
+
+// Populating event on load
 function handleSuccess(json){
   eventData = json;
   renderEvent();
@@ -90,11 +122,22 @@ function handleError(err) {
   $eventTarget.text('Failed to render Event');
 }
 
+// Adding new suggestion
 function suggestionSuccess(json){
   suggestionData = json;
   renderSuggestion();
 }
 function suggestionError(err){
   console.log('events.js: suggestion error: ', err);
+  $suggestionTarget.text('Failed to render Suggestion');
+}
+
+// Populating suggestions on load
+function suggOnLoadSuccess(json){
+  suggOnLoadData = json;
+  renderSuggestions();
+}
+function suggOnLoadError(err){
+  console.log('events.js: suggOnLoad error: ', err);
   $suggestionTarget.text('Failed to render Suggestions');
 }
